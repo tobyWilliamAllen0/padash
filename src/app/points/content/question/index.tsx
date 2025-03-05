845d743a679c628f3847f0388406bde60bc9fe07
Kevin <kevin.sherry@craftomatic.de>
2025-03-05 20:53:11 +0000

    implement question api

---
+++ src/app/points/content/question/index.tsx
@@ -0,0 +1,80 @@
+import dayjs from 'dayjs';
+import { useTimer } from 'react-timer-hook';
+import duration from 'dayjs/plugin/duration';
+import { useEffect, useMemo } from 'react';
+import useFetch from '@/hooks/useFetch';
+import CheckboxGroup from '@/components/CheckboxGroup';
+
+dayjs.extend(duration);
+
+const getDateNow = () => new Date();
+
+export const Question = () => {
+	const [questionState, fetchQuestion] = useFetch();
+
+	const EXPIRY_TIMESTAMP = useMemo(() => {
+		const EXPIRY_TIMESTAMP = questionState?.response
+			? questionState?.response[0].active_time
+			: getDateNow();
+
+		EXPIRY_TIMESTAMP.setHours(EXPIRY_TIMESTAMP.getHours() + 8);
+
+		return EXPIRY_TIMESTAMP;
+	}, [questionState?.response]);
+
+	const { seconds, isRunning, pause, minutes, hours } = useTimer({
+		expiryTimestamp: questionState?.response
+			? questionState?.response[0].active_time
+			: EXPIRY_TIMESTAMP,
+		onExpire: () => pause(),
+	});
+
+	const handleGetSelectedValues = (value: string) => {
+		console.log('Selected Values:', value);
+	};
+
+	useEffect(() => {
+		fetchQuestion({
+			url: 'admin/question',
+			method: 'GET',
+		});
+	}, []);
+
+	console.log(questionState, 'questionState');
+
+	return (
+		<div>
+			{isRunning && (
+				<div className="flex items-center justify-end gap-4">
+					<span>زمان باقی مانده:</span>
+					<span>
+						{' '}
+						{dayjs
+							.duration({
+								seconds,
+								minutes,
+								hours,
+							})
+							.format('HH[:]mm[:]ss')}
+					</span>
+				</div>
+			)}
+
+			{questionState.response && (
+				<>
+					<div className="mt-4">
+						<span>{questionState?.response[0].question}</span>
+					</div>
+					<div className="mt-4">
+						<CheckboxGroup
+							onChange={handleGetSelectedValues}
+							options={questionState?.response[0].options.map(
+								(option: string) => ({ label: option, value: option }),
+							)}
+						/>
+					</div>
+				</>
+			)}
+		</div>
+	);
+};
--- src/app/points/page.tsx
+++ src/app/points/page.tsx
@@ -1,16 +1,11 @@
 'use client';
 
-import CheckboxGroup from '@/components/CheckboxGroup';
 import FlipCardGame from '@/components/flipCardGame';
 import Tab from '@/components/Tab';
-import useFetch from '@/hooks/useFetch';
 import { useEffect, useMemo, useRef } from 'react';
+import { Question } from './content/question';
 
 export default function Points() {
-	const checkboxGroupRef = useRef<{ getSelectedValues: () => string[] }>(null);
-
-	const [questionState, fetchQuestion] = useFetch();
-
 	const tabs = useMemo(
 		() => [
 			{
@@ -23,47 +18,12 @@ export default function Points() {
 			},
 			{
 				label: 'سوال',
-				content: (
-					<div>
-						<div className="flex items-center justify-end gap-4">
-							<span>زمان باقی مانده:</span>
-							<span>48:00:00</span>
-						</div>
-
-						{questionState.response && (
-							<>
-								<div className="mt-4">
-									<span>{questionState?.response[0].question}</span>
-								</div>
-								<div className="mt-4">
-									<CheckboxGroup
-										onChange={handleGetSelectedValues}
-										options={questionState?.response[0].options.map(
-											(option: string) => ({ label: option, value: option }),
-										)}
-									/>
-								</div>
-							</>
-						)}
-					</div>
-				),
+				content: <Question />,
 			},
 		],
 		[],
 	);
 
-	const handleGetSelectedValues = (value: string) => {
-		console.log('Selected Values:', value);
-	};
-
-	useEffect(() => {
-		fetchQuestion({
-			url: 'admin/question',
-			method: 'GET',
-		});
-	}, []);
-
-	console.log(questionState, 'questionState');
 	return (
 		<div className="p-4 overflow-auto" style={{ height: 'calc(100vh - 81px)' }}>
 			<div className="w-full flex flex-col justify-right items-start mt-4 gap-2 mb-6">
